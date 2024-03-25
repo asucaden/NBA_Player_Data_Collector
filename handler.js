95f693fce31a97cc92c9d56c57ce0f58e2a8a7a1
Caden Midkiff <cadenmidkiff32@gmail.com>
2024-03-24 22:42:02 -0700

    Added closing log note to verify lambdas

--- handler.js
+++ handler.js
@@ -1,14 +1,15 @@
-'use strict';
+"use strict";
 
-const connectDB = require('./config/db');
-const { yearlyPlayerSync, dailyStatsUpdate } = require('./updateStats');
+const connectDB = require("./config/db");
+const { yearlyPlayerSync, dailyStatsUpdate } = require("./updateStats");
 
 module.exports.DailyUpdate = async (event) => {
   await connectDB();
   await dailyStatsUpdate();
+  console.log("Daily Stats Update promise is returned");
 
   return {
-    message: 'Stats updated!',
+    message: "Stats updated!",
     event,
   };
 };
